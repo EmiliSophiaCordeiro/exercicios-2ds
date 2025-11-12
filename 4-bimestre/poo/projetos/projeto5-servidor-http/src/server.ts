2c4ebeb7fa384ab641a22de4eaf52490220ebad0
✧ Sophia Cordeiro✧ <emilisophiassss@gmail.com>
2025-11-12 11:22:34 +0000

    terminado

---
+++ 4-bimestre/poo/projetos/projeto5-servidor-http/src/server.ts
@@ -0,0 +1,20 @@
+import http from 'http'
+
+const PORT = Number(process.env.PORT ?? 3001)
+const NAME = 'John'
+const AGE = 34
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+  res.end(`Servidor rodando para ${NAME}, idade ${AGE}!`)
+})
+
+server.listen(PORT, () => {
+  console.log(`Servidor funcionando em http://localhost:${PORT}`)
+})
+
+server.close(() => {
+  console.log('Servidor encerrado.')
+})
+
+export default server
